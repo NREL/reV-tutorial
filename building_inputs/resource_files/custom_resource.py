f89b9988cb983ee880993378c469e2eba9fc6738
hwang6 <hwang6@nrel.gov>
2024-07-30 10:55:52 -0500

    fixed *.srw file input issues

--- building_inputs/resource_files/custom_resource.py
+++ building_inputs/resource_files/custom_resource.py
@@ -40,10 +40,13 @@ def reformat_single(file):
     with open(file, "r") as f:
         reader = csv.reader(f, delimiter=",")
         for i, line in enumerate(reader):
-
             if (i == 0):
                 # In this case we have unit and location information in the first 2 lines
+                # HW 07292024 update: as in the SAM documentation: Row 1 must have at least 8 columns. You must provide a value for each column:
+                # If you do not have a value for a column, you can use an indicator like n/a or ?? for the missing value.
+                # Since we have missed original inputs, we generated a value for "year" from the output *.h5 file. 
                 gid, city, state, county, year, lat, lon, _, _, steps = line
+                year = 2013
 
             if (i == 1):
                 # More descriptive info is found in the second row, first column
@@ -77,10 +80,7 @@ def reformat_single(file):
     data = data.astype(float)
 
     # Hold onto the meta information
-    # HW 07292024 update: as in the SAM documentation: Row 1 must have at least 8 columns. You must provide a value for each column:
-    # If you do not have a value for a column, you can use an indicator like n/a or ?? for the missing value.
-    # Since we have missed original inputs, we generated a value for "year" from the output *.h5 file. 
-    meta = {"latitude": lat, "longitude": lon, "year": 2013, 
+    meta = {"latitude": lat, "longitude": lon, "year": year, 
             "timesteps": steps, "description": description}
     return data, meta
 
