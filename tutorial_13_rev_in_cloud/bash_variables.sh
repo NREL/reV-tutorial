122e2dd3778fca474a506764f49fc65522f80148
AWS ParallelCluster user <ubuntu@ip-172-18-30-186.us-west-2.compute.internal>
2025-10-15 15:37:15 +0000

    Added a small file with required bash variables for the AWS pcluster tutorial.

---
+++ tutorial_13_rev_in_cloud/bash_variables.sh
@@ -0,0 +1,9 @@
+
+# AWS Parameters
+export AWS_REGION="us-west-2"
+export AWS_S3_GATEWAY="http://s3.us-west-2.amazonaws.com/"
+export AWS_S3_NO_SIGN_REQUEST=1
+
+# HSDS PARAMETERS
+export BUCKET_NAME="nrel-pds-hsds"
+export HSDS_ENDPOINT="http://localhost:5101"
